837027d2aa31ef92308327784defca6a0d9e3b05
ibo-ibo <zihnimveben@gmail.com>
2022-07-08 08:22:35 +0300

    project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <quote-item
+      @get-quote="getQuote"
+      :anime-character="character"
+      :anime-name="name"
+      :anime-quote="quote"
+    ></quote-item>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import QuoteItem from "./components/QuoteItem.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { QuoteItem },
+  data() {
+    return {
+      name: "",
+      quote: "",
+      character: "",
+    };
+  },
+  methods: {
+    async getQuote() {
+      const res = await fetch("https://animechan.vercel.app/api/random");
+      const data = await res.json();
+      this.name = data.anime;
+      this.quote = data.quote;
+      this.character = data.character;
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Raleway", sans-serif;
+}
+
+body {
+  position: relative;
+  background-color: #ffa8a8;
+  padding: 3rem;
+  color: #212529;
 }
 </style>
---
+++ src/components/QuoteItem.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="quote-wrapper">
+    <header>
+      <h1>{{ animeName }}</h1>
+    </header>
+    <div class="quote">
+      <p class="quote-text">{{ animeQuote }}</p>
+      <p class="quote-character">{{ animeCharacter }}</p>
+    </div>
+    <button @click="getQuote">Get Quote</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["anime-name", "anime-quote", "anime-character"],
+  emits: ["get-quote"],
+  methods: {
+    getQuote() {
+      this.$emit("get-quote");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.quote-wrapper {
+  padding: 2rem 1rem;
+  margin-top: 50px;
+  background-color: #fa5252;
+  display: flex;
+  flex-direction: column;
+}
+
+header {
+  margin-bottom: 12px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+h1 {
+  text-shadow: 2px 2px 3px rgba(0, 0, 0, 0.3);
+}
+
+.quote {
+  padding: 1rem 1rem;
+  margin-bottom: 32px;
+}
+
+.quote-text {
+  margin-bottom: 12px;
+  font-size: 24px;
+}
+
+.quote-character {
+  color: #f8f9fa;
+}
+
+button {
+  padding: 1rem 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: none;
+  outline: none;
+  border: none;
+  background-color: #5f3dc4;
+  border-radius: 4px;
+  font-weight: 500;
+  font-size: 18px;
+  margin: 0 auto;
+  cursor: pointer;
+}
+button:hover {
+  opacity: 0.8;
+}
+</style>
